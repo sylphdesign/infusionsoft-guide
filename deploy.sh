fc4c6629993f4795330e8eef596e377ca03fdfb0
sylphdesign <dpatel@omazingcreations.com>
2018-09-24 07:58:19 -0700

    deploy script update

--- deploy.sh
+++ deploy.sh
@@ -20,6 +20,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:sylphdesign/inf-docs.git master:gh-pages
+git push -f git@github.com:sylphdesign/infusionsoft-guide.git master:gh-pages
 
 cd -
